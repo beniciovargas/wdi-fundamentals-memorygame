d9b811545575de61c3bf978a02551297553da40d
benicio vargas <beniciorvargas@gmail.com>
2020-01-17 09:49:53 -0800

    updated js code

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,23 +1,50 @@
-let cards = ["queen", "queen", "king", "king"];
-let cardsInPlay = [];
+const cards =[
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	}, 
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	}, 
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	},  
+];
+
+
+cardsInPlay=[];
 
 function checkForMatch(){
 	if (cardsInPlay[0] === cardsInPlay[1]){
-		alert("You have found a match!")
-	}else{
-		alert ("Sorry, try again.")
-	};
+				console.log("You found a match!");
+			} else{
+				console.log ("Sorry, try again.");
+			};
 };
+
 
 function flipCard(cardId){
-	console.log ("User flipped "+ cards[cardId])
-	cardsInPlay.push(cards[cardId]);
+	
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+
+	cardsInPlay.push(cards[cardId].rank);
+
 	if (cardsInPlay.length === 2){
 		checkForMatch();
-	
-	}else{
-		//do something else
-	};
+	}
 };
 
-flipCard();
+flipCard(0);
+flipCard(2);
